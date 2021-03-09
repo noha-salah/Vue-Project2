a3e21bc9e50cda1e649313b478e53477ed172168
noha salah <lifeforever633.com>
2021-03-09 20:55:57 +0200

    add vue project

---
+++ modal-project2/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+<h1>{{ title }}</h1>
+<teleport  to=".noha" v-if="showmodal">
+  
+  <Modal :header="header" :text="text" theme="sale" @close="togglemodal"> 
+  <template v-slot:links>
+<a href="">sign up</a>
+<a href=""> more info </a>
+
+
+  </template>
+  
+  <h2>slotdefault</h2>
+  <p>solt default in details</p>
+  
+  </Modal>
+  
+  </teleport>
+  <div v-if="showmodal2">
+  
+  <Modal :header="header" :text="text" theme="" @close="togglemodal2"> 
+  <template v-slot:links>
+
+  </template>
+  
+  <h2>newsletter</h2>
+  <p>solt default in details</p>
+  
+  </Modal>
+  
+  </div>
+  <button @click="togglemodal">Open Model</button>
+   <button @click="togglemodal2">Open Model2</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  data(){
+return {
+  title :'Vue Project  noha:)',
+  header:'header for modal',
+  text:'text in model',
+  showmodal:false,
+  showmodal2:false
+}
+  },
+  methods:{
+togglemodal(){
+  this.showmodal=!this.showmodal
+},togglemodal2(){
+  this.showmodal2=!this.showmodal2
+}
+  },
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ modal-project2/src/components/Modal.vue
@@ -0,0 +1,61 @@
+
+<template>
+    
+<div id="myModal" class="modal" @click.self="closemodal" >
+
+  <!-- Modal content -->
+  <div class="modal-content" :class="{ sale:theme === 'sale' }">
+
+    <p>Some text in the Modal..</p>
+    <p>{{ header }}</p>
+    <p>{{ text }}</p>
+    <slot></slot>
+    <slot name="links"></slot>
+  </div>
+
+</div>
+
+
+</template>
+<script>
+export default{
+    props:['header','text','theme'],
+    methods:{
+closemodal(){
+    this.$emit('close')
+}
+
+    }
+}
+
+</script>
+<style>
+ /* The Modal (background) */
+.modal {
+   /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+.sale{
+background-color: rgb(214, 144, 211); /* Fallback color */
+
+}
+
+/* The Close Button */
+</style>
