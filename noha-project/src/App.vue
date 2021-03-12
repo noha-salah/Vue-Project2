60130e12ec2d74ea487c8dcfedeb9208bc5dfa8d
noha salah <lifeforever633.com>
2021-03-12 19:02:33 +0200

    add new project

---
+++ noha-project/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<h1>Noha Project (try  in rection-timer ) </h1>
+<button @click="start">Play </button>
+<Block v-if="isplaying"/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+export default {
+  name: 'App',
+ data(){
+   return {
+   isplaying:false,
+   delay:null
+   }
+
+
+ },components:{Block},
+ methods:{
+   start(){
+     this.delay=2000+Math.random()*5000
+this.isplaying=!this.isplaying
+
+   }
+
+ }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ noha-project/src/components/Block.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="block" v-if="showBlock">
+
+      click me 
+  </div>
+</template>
+
+<script>
+export default {
+props:['delay'],
+data(){
+    return 
+    {
+        showBlock:false
+        timer:null
+        reactiontime: 0
+    }
+},
+mounted(){
+setTimeout(() => {
+    this.showBlock= true
+   
+}, this.delay)
+
+},
+methods:{
+starttimer(){
+this.timer = setInterval(() => {
+   this.reactiontime +=10 
+}, 10)
+},
+stoptimer() {
+clearInterval(this.timer)
+}
+
+}
+
+}
+</script>
+
+<style>
+
+</style>
